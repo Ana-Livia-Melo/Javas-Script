f350634998373444d08c1e8bef06996f160b8a5d
Ana Lívia <141974443+Ana-Livia-Melo@users.noreply.github.com>
2023-10-27 20:31:19 -0300

    Add files via upload

---
+++ JAVASCRIPT/script1.js
@@ -0,0 +1,14 @@
+let resultado = 10 + 5;
+let diferenca = 20 - 8;
+let produto = 6 * 4;
+let quociente = 15 / 3;
+let resto = 10 % 3;
+
+<input type = 'button' name='btn' id='btn' value='clique aqui'></input>
+
+let nome = prompt ("Digite seu Nome");
+let idade = prompt ("Digite sua idade");
+
+
+COLOCAR 
+Document.WRITE("")
